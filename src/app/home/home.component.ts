3d91aa53ed82e5b083a400256fbd0e57e72aba69
Agustin Ruiz <agusvaldes2@live.com>
2018-03-09 01:35:14 -0600

    Add items in array

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  listCount: number;
+  btnAdd: string = 'Add item';
+  goalText: string = '';
+  goals = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.listCount = this.goals.length;
+  }
+
+  addGoal (){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.listCount = this.goals.length;
   }
 
 }
